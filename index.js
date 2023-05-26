db29d6582dd4281f950f91e514e388a73e24829b
minjiBaek <emilyback@naver.com>
2023-05-27 00:13:21 +0900

    불꽃놀이 꼬리 추가 - 일정 폭 간격 사이로만 랜덤하게 시작 - 위로 올라갈 수 록 옅게 - 처음엔 가로로 움직이며 올라가다 곧게 끝 - 속도 랜덤 - slice -> splice 오타 수정

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
+import Tail from "./js/Tail.js";
 import { randomNumBetween, hypotenuse } from "./js/utils.js";
 
 class Canvas extends CanvasOption{
   constructor(){
     super();// 부모 class 모든 요소 및 함수 사용
-
+    
+    this.tails = [];
     this.particles = [];
   }
 
@@ -18,13 +20,19 @@ class Canvas extends CanvasOption{
     this.canvas.style.width = this.canvasWidth + 'px';
     this.canvas.style.height = this.canvasHeight + 'px';
 
+
     this.createParticles();
   }
 
-  createParticles() {
+  createTails(){
+    const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+    const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
+    const color = '255, 255, 255';
+    this.tails.push(new Tail(x, vy, color));
+  }
+
+  createParticles(x, y, color) {
     const PARTICLE_NUM  = 400;
-    const x = randomNumBetween(0, this.canvasWidth);
-    const y = randomNumBetween(0, this.canvasHeight);
     for(let i = 0; i < PARTICLE_NUM ; i ++){
       //현재 x,y rand 값을 5~ -5로 했을때 퍼지는 모양이 사각형이였음
       // 원의 모양을 만들기 위해 x, y 사이 값을 원의 반지름 값으로 날아 갈 수 있도록 해야된다.
@@ -34,7 +42,7 @@ class Canvas extends CanvasOption{
       const vx = r * Math.cos(angle);
       const vy = r * Math.sin(angle);
       const opacity = randomNumBetween(0.6 , 0.9 );
-      this.particles.push(new Particle(x, y, vx, vy, opacity ));
+      this.particles.push(new Particle(x, y, vx, vy, opacity, color ));
     }
   }
 
@@ -53,12 +61,25 @@ class Canvas extends CanvasOption{
         this.ctx.fillStyle = this.bgColor + '40' ;
         this.ctx.fillRect(0,0, this.canvasWidth, this.canvasHeight);
 
+      if(Math.random() < 0.03) 
+        this.createTails();
+
+      this.tails.forEach((tail, index)=> {
+        tail.update();
+        tail.draw();
+
+        if(tail.vy > -0.7){
+          this.tails.splice(index, 1);
+          this.createParticles(tail.x, tail.y, tail.color);
+        }
+      })
+
       this.particles.forEach((particle, index) => {
         particle.update();
         particle.draw();
 
         if(particle.opacity < 0 ){
-          this.particles.slice(index, 1);
+          this.particles.splice(index, 1);
         }
       })
       then = now - (delta % this.interval);
--- js/Particle.js
+++ js/Particle.js
@@ -1,15 +1,16 @@
 import CanvasOption from "./CanvasOption.js";
 
 export default class Particle extends CanvasOption{
-  constructor(x,y, vx, vy, opacity){
+  constructor(x,y, vx, vy, opacity, color){
     super();
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
     this.opacity = opacity;
+    this.color = color
     this.gravity = 0.12;
-    this.friction = 0.93; //속도 조절, 점점 0에 수렴
+    this.friction = 0.985; //속도 조절, 점점 0에 수렴
   }
   update(){
     this.vy += this.gravity;
@@ -22,7 +23,7 @@ export default class Particle extends CanvasOption{
     this.opacity -= 0.02;
   }
   draw(){
-    this.ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`
+    this.ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
     this.ctx.fill();
