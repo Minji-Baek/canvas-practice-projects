a32fba144795edea6aed90fc36a91831217d0f13
minjiBaek <emilyback@naver.com>
2023-05-31 00:44:08 +0900

    밑으로 떨어지는 하나의 콘페티 만듬 -ctx.translate(x,y)로 x,y축을 변경해 rotate 축을 콘페티 자신으로 변경 후 바로 -x, -y 로 변경 -rotate계속 돌고있음으로 resetTransform해주기

--- confetti-effect/index.js
+++ confetti-effect/index.js
@@ -19,6 +19,15 @@ function init() {
  function render(){
     let now, delta;
     let then = Date.now();
+    const x = innerWidth / 2;
+    let y = innerHeight / 2;
+
+    const width = 50;
+    const height = 50;
+    let deg = 0.1;
+
+
+    let widthAlpah = 0; // 가중치
 
     const frame = ()=> {
       requestAnimationFrame(frame);
@@ -27,6 +36,20 @@ function init() {
       delta = now - then;
     
       if(delta < interval) return 
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+      widthAlpah += 0.1;
+      deg += 0.1;
+      y += 1; // 밑으로 떨어짐 표현
+      ctx.translate(x + width, y + height);
+      ctx.rotate(deg); // 
+      ctx.translate(-(x + width), -(y + height));
+      // 3d 입체감을 줌
+
+      ctx.fillStyle = 'red'
+      ctx.fillRect(x, y, width * Math.cos(widthAlpah), height * Math.sin(widthAlpah)); // 좌우로 펄럭거림 표현
+
+      ctx.resetTransform();
         
       then = now - (delta % interval);
 
