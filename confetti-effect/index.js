2d643ca7d13e057ef84a32af05af26c97a4bd1ad
minjiBaek <emilyback@naver.com>
2023-05-31 21:28:04 +0900

    콘페티가 좌측에서 우측으로 터치는 effect 느낌까지 제작

--- confetti-effect/index.js
+++ confetti-effect/index.js
@@ -1,3 +1,5 @@
+import Particle from "./js/Particles.js";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const dpr = devicePixelRatio > 1 ? 2 : 1
@@ -5,6 +7,9 @@ const fps = 70;
 const interval = 1000 / fps;
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
+
+const particles = [];
+
 
 function init() {
     canvasHeight = innerHeight;
@@ -14,20 +19,26 @@ function init() {
     canvas.width = canvasWidth * dpr;
     canvas.height = canvasHeight * dpr;
     ctx.scale(dpr, dpr);
+
+    
+    createConfetti({
+      x: canvasWidth / 2,
+      y: canvasHeight / 2,
+      count: 10
+    });
+  }
+
+
+  function createConfetti({x, y, count, deg}){
+    for(let i = 0; i < count ; i++){
+      particles.push(new Particle(x, y, deg))
+    }
   }
 
  function render(){
     let now, delta;
     let then = Date.now();
-    const x = innerWidth / 2;
-    let y = innerHeight / 2;
-
-    const width = 50;
-    const height = 50;
-    let deg = 0.1;
-
-
-    let widthAlpah = 0; // 가중치
+  
 
     const frame = ()=> {
       requestAnimationFrame(frame);
@@ -38,26 +49,25 @@ function init() {
       if(delta < interval) return 
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-      widthAlpah += 0.1;
-      deg += 0.1;
-      y += 1; // 밑으로 떨어짐 표현
-      ctx.translate(x + width, y + height);
-      ctx.rotate(deg); // 
-      ctx.translate(-(x + width), -(y + height));
-      // 3d 입체감을 줌
-
-      ctx.fillStyle = 'red'
-      ctx.fillRect(x, y, width * Math.cos(widthAlpah), height * Math.sin(widthAlpah)); // 좌우로 펄럭거림 표현
-
-      ctx.resetTransform();
-        
+      for(let i = particles.length - 1; i >= 0; i--){
+        particles[i].update();
+        particles[i].draw(ctx);
+      }
+     
       then = now - (delta % interval);
 
     }
     requestAnimationFrame(frame);
   }
 
-
+  window.addEventListener('click', () => {
+    createConfetti({
+      x: 0,
+      y: canvasHeight / 2,
+      count: 10,
+      deg: -50
+    });
+  });
 
 window.addEventListener('load', () => {
   init();
---
+++ confetti-effect/js/Particles.js
@@ -0,0 +1,36 @@
+import { randomNumBetween } from "./utils.js";
+
+export default class Particle {
+  constructor(x, y, deg = 0){
+    this.angle = Math.PI / 180 * randomNumBetween(deg - 30, deg + 30); // deg 중심 부채꼴 뿌리기
+
+    this.x = x;
+    this.y = y;
+    this.r = randomNumBetween(30, 100);
+
+    
+    this.vx = this.r * Math.cos(this.angle); // 가중치?이동값? 더하기
+    this.vy = this.r * Math.sin(this.angle); // 가중치 더하기
+
+    // this.friction = randomNumBetween(0.95, 0.97); //마찰력 곱
+    this.friction =  0.9; //마찰력 곱
+
+    this.gravity = 0.5; //중력 더하기
+    
+    this.width = 30;
+    this.height = 30;
+  }
+  update(){
+    this.vy += this.gravity;
+
+    this.vy *= this.friction;
+    this.xy *= this.friction;
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  draw(ctx){
+    ctx.fillStyle = `red`
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
