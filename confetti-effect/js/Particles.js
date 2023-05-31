27bfcc5f9376782144636bc79aa499af53380f83
minjiBaek <emilyback@naver.com>
2023-05-31 21:29:15 +0900

    특정 각도 지정하여 폭죽과 다르게 콘페티가 발사되는 느낌을 줌

--- confetti-effect/js/Particles.js
+++ confetti-effect/js/Particles.js
@@ -2,13 +2,13 @@ import { randomNumBetween } from "./utils.js";
 
 export default class Particle {
   constructor(x, y, deg = 0){
-    this.angle = Math.PI / 180 * randomNumBetween(deg - 30, deg + 30); // deg 중심 부채꼴 뿌리기
+    this.angle = Math.PI / 180 * randomNumBetween(deg - 30, deg + 30); // deg 중심 부채꼴 뿌리기 특정 각도 지정
 
     this.x = x;
     this.y = y;
     this.r = randomNumBetween(30, 100);
 
-    
+     
     this.vx = this.r * Math.cos(this.angle); // 가중치?이동값? 더하기
     this.vy = this.r * Math.sin(this.angle); // 가중치 더하기
 
