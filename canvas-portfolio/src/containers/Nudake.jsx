7897954234973185ed6a739a5e79bd83775c0eb1
minjiBaek <emilyback@naver.com>
2023-06-05 23:41:48 +0900

    mouse down, move, up event를 생성

--- canvas-portfolio/src/containers/Nudake.jsx
+++ canvas-portfolio/src/containers/Nudake.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useRef } from 'react';
 import '../style/containers/Nudake.css';
 
+import image1 from '../assets/nudake-1.jpg';
+import image2 from '../assets/nudake-2.jpg';
+import image3 from '../assets/nudake-3.jpg';
+
+
 const Nudake = () => {
   const canvasRef = useRef(null); // return 되기 전에 document의 element를 가져올 수 없어 변수만 선언 후 null
   
@@ -11,6 +16,9 @@ const Nudake = () => {
     const canvas = canvasRef.current;
     const cavasParnet = canvas.parentNode;
     const ctx = canvas.getContext('2d');
+
+    const imageSrcs = [image1, image2, image3];
+    let currentIndex = 0;
 
     let canvasWidth, canvasHeight;
 
@@ -23,13 +31,43 @@ const Nudake = () => {
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
 
+      drawImage();
     }
+
+    function drawImage() {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      const image = new Image();
+      image.src = imageSrcs[currentIndex];
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight); 
+      }
+    }
+
+    const onMouseDown = ()=> {
+      console.log('onMouseDown')
+      canvas.addEventListener('mouseup', onMouseUp);
+      canvas.addEventListener('mousemove', onMouseMove);
+    }
+    const onMouseUp = ()=> {
+      console.log('onMouseUp')
+      canvas.removeEventListener('mouseup', onMouseUp);
+      canvas.removeEventListener('mousemove', onMouseMove);
+    }
+    const onMouseMove = ()=> {
+      console.log('onMouseMove')
+    }
+    canvas.addEventListener('mousedown', onMouseDown);
+    
+
 
     window.addEventListener('resize', resize)
     resize();
 
     return () => { //amount 될때 cleanUp 항목들
-      window.removeEventListener('resize', resize)
+      window.removeEventListener('resize', resize);
+      canvas.removeEventListener('mousedown', onMouseDown);
+    
+
     }
 
   }, [])
