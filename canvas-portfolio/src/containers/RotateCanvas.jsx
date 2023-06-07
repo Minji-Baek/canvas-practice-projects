5fd048ba1a9f6ad41d23456296250f2caae4a60b
minjiBaek <emilyback@naver.com>
2023-06-08 00:28:31 +0900

    원 안에 item 들 계속 돌아가는 motion 적용, item click 하면 정보 표출

--- canvas-portfolio/src/containers/RotateCanvas.jsx
+++ canvas-portfolio/src/containers/RotateCanvas.jsx
@@ -1,13 +1,32 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import '../style/containers/RotateCanvas.css'
-import {Engine, Render, Runner, Mouse, MouseConstraint, Composite, Bodies} from 'matter-js'
+import {Engine, Render, Runner, Mouse, MouseConstraint, Composite, Bodies, Events} from 'matter-js'
+import IconAFRAME from '../assets/icon_AFRAME.png';
+import IconHTML from '../assets/icon_HTML.png';
+import IconCSS from '../assets/icon_CSS.png';
+import IconJS from '../assets/icon_JS.png';
+import IconREACT from '../assets/icon_REACT.png';
+import IconTHREE from '../assets/icon_THREE.png';
+
+const data = {
+  'JS': { title: 'Javascript', level: 4, desc: '자바스크립트에 대한 설명이라고 할 수 있습니다. 자바스크립트에 대한 설명. 자바스크립트에 대한 설명.' },
+  'REACT': { title: 'React.js', level: 5, desc: 'React에 대한 설명이라고 할 수 있습니다. React에 대한 설명. React에 대한 설명.' },
+  'CSS': { title: 'CSS/SASS', level: 3, desc: 'CSS에 대한 설명이라고 할 수 있습니다. CSS에 대한 설명. CSS에 대한 설명.' },
+  'AFRAME': { title: 'Aframe.js', level: 4, desc: 'AFRAME에 대한 설명이라고 할 수 있습니다. AFRAME에 대한 설명. AFRAME에 대한 설명.' },
+  'THREE': { title: 'Three.js', level: 2, desc: 'THREE에 대한 설명이라고 할 수 있습니다. THREE에 대한 설명. THREE에 대한 설명.' },
+  'HTML': { title: 'HTML', level: 5, desc: 'HTML에 대한 설명이라고 할 수 있습니다. HTML에 대한 설명. HTML에 대한 설명.' },
+}
 
 const RotateCanvas = () => {
+  const [selected, setSelected] = useState(data['JS'])
   const canvasRef = useRef(null);
   useEffect(()=>{
     const canvas = canvasRef.current;
     const cw = 1000;
     const ch = 1000;
+
+    const gravityPower = 0.5;
+    let gravityDeg = 0;
 
     let engine, render, runner, mouse, mouseConstraint;
  
@@ -35,6 +54,29 @@ const RotateCanvas = () => {
       const rect = Bodies.rectangle(x, y, w, h, options);
       Composite.add(engine.world, rect);
     }
+    const initImageBoxes = () =>{
+      const scale = 0.7;
+      const t1 = { w: 250 * scale, h: 250 * scale }
+      const t2 = { w: 732 * scale, h: 144 * scale }
+      addRect(cw/2, ch/2, t1.w, t1.h,
+        { label:'JS', chamfer: {radius: 20}, render: {sprite: {texture: IconJS, xScale: scale, yScale: scale}}});
+
+      addRect(cw/2 - t1.w , ch/2, t1.w, t1.h, 
+        { label:'HTML', chamfer: {radius: 20}, render: {sprite: {texture: IconHTML, xScale: scale, yScale: scale}}});
+
+      addRect(cw/2 + t1.w, ch/2, t1.w, t1.h, 
+        { label:'CSS', chamfer: {radius: 20}, render: {sprite: {texture: IconCSS, xScale: scale, yScale: scale}}});
+
+      addRect(cw/2, ch/2 + t1.h, t1.w, t1.h, 
+        { label:'REACT', chamfer: {radius: 75}, render: {sprite: {texture: IconREACT, xScale: scale, yScale: scale}}});
+
+      addRect(cw/2 - t1.w, ch/2 + t1.h, t1.w, t1.h, 
+        { label:'THREE', chamfer: {radius: 20}, render: {sprite: {texture: IconTHREE, xScale: scale, yScale: scale}}});
+
+      addRect(cw/2, ch/2 - t2.h, t2.w, t2.h, 
+        { label:'AFRAME', chamfer: {radius: 20}, render: {sprite: {texture: IconAFRAME, xScale: scale, yScale: scale}}});
+    }
+
     const initGround = () =>{ 
       // matter.js에서 원의 형태의 ground가 없음으로 다각형 ground를 만들어준다.
       // 다각형 지점에 rect를 만들어 버린다.
@@ -55,19 +97,30 @@ const RotateCanvas = () => {
     initScene();
     initMouse();
     initGround();
+    initImageBoxes();
 
-    canvas.addEventListener('mousewheel', ()=>{
-      addRect(mouse.position.x, mouse.position.y, 50, 50);
-    });
+    Events.on(mouseConstraint, 'mousedown', ()=> {
+      const newSelected = mouseConstraint.body && data[mouseConstraint.body.label]
+      newSelected && setSelected(newSelected);
+    })
+    
+    Events.on(runner, 'tick', ()=>{
+      gravityDeg += 1;
+      engine.world.gravity.x = gravityPower * Math.cos(Math.PI / 180 * gravityDeg);
+      engine.world.gravity.y = gravityPower * Math.sin(Math.PI / 180 * gravityDeg);
+
+    })
 
   },[])
   return (
     <div className="rotate-canvas-wrapper">
       <canvas ref={canvasRef}></canvas>
       <aside>
-        <h1>Javascript</h1>
-        <h2>★★★★★</h2>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        <h1>{selected.title}</h1>
+        <h2>{Array(5).fill(null).map((_, i) => (
+          <span key={i} style={{ filter: `grayscale(${selected.level <= i ? 1 : 0})` }}>&#11088;</span>
+        ))}</h2>
+        <p>{selected.desc}</p>
       </aside>
     </div>
   )
