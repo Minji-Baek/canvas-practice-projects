9bcb47f8c0d0b3efae10aab9688e3fee31f4af44
minjiBaek <emilyback@naver.com>
2023-06-15 14:59:42 +0900

    미니게임 일단 마무리 추후 업데이트

--- canvas-portfolio/src/containers/MiniGame.jsx
+++ canvas-portfolio/src/containers/MiniGame.jsx
@@ -8,11 +8,12 @@ import blueBird from '../assets/minigame/blue-bird.png'
 import coin from '../assets/minigame/coin.png'
 import play from '../assets/minigame/play.svg'
 import replay from '../assets/minigame/replay.svg'
-import title from '../assets/minigame/title.png'
+import title from '../assets/minigame/title2.png'
 import wall from '../assets/minigame/Wall.png'
+import gsap from 'gsap';
 
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { randomNumBetween } from '../utils/utils'
 
 const canvasWidth = 1024;
@@ -23,7 +24,15 @@ const interval = 1000 / fps;
 const MiniGame = () => {
 
   const canvasRef = useRef(null); 
-
+  let gameHandler; 
+  const [handlerStatus, setHandlerState] = useState('READY');
+  useEffect(()=> {
+    if(gameHandler !== undefined) //여기부터
+      switch (handlerStatus){
+        case 'READY': gameHandler.showReadyScreen(); break;
+        case 'FINISH': gameHandler.showFinishScreen(); break;
+      }
+  }, [handlerStatus])
 
   useEffect(()=>{
     let frameId;
@@ -39,14 +48,10 @@ const MiniGame = () => {
     const backbroundImageSrcs = [bg1,bg2, bg3];
     let backgroundLoaded = [];
 
-    const imageSrcs = [coin,play, replay,  title ];
-    let imageLoaded = [];
-
     let walls = []
     let backgrounds = [];
     let coins = [];
-    let scores = [];
-    // this.gameHandler = new GameHandler(this);
+    let score;
     let player;
     function resize(){
       //background image setting 
@@ -55,13 +60,134 @@ const MiniGame = () => {
         backgrounds.push(createBackground({img :backgroundLoaded[1], speed: -2}))  
         backgrounds.push(createBackground({img :backgroundLoaded[0], speed: -4}))  
       });
-     
+      gameHandler = setGameHandler();
     }
+
+    const setGameHandler = () => {
+      setHandlerState(current=> current = "READY")
+
+      const titleImg = new Image();
+      const playImg = new Image();
+      const replayImg = new Image();
+      // const coinImg = new Image();
+      const status = handlerStatus;
+      const readyScreen = document.querySelector('.ready-screen');
+      const finishScreen = document.querySelector('.finish-screen');
+      const distanceText = finishScreen.querySelector('.distance');
+      const coinText = finishScreen.querySelector('.coin');
+
+
+      titleImg.src = title;
+      playImg.src = play;
+      replayImg.src = replay;
+      // coinImg.src = coin;
+
+      readyScreen.appendChild(titleImg);
+      readyScreen.appendChild(playImg);
+      // const initHandler = () => {
+
+      // }
+
+
+      const titleImgae = readyScreen.childNodes[0]
+      titleImgae.setAttribute('className', "title-img");
+
+      const playBtn = readyScreen.childNodes[1]
+      playBtn.setAttribute('className', "play-img");
+      const hideReadyScreen=()=>{
+        gsap.to(readyScreen,{
+          opacity: 0, pointerEvents: 'none', duration: 0.3, onComplete: () => {
+            setHandlerState(current=> current = "PLAYING")
+          }
+        })
+      }
+      playBtn.addEventListener('click', () => {
+        hideReadyScreen();
+      })
+
+     
+      finishScreen.appendChild(replayImg);
+
+
+      const replayBtn = finishScreen.childNodes[2];
+      // console.log
+      replayBtn.setAttribute('className','replay-img');
+      const hidieFinishScreen = () => {
+        gsap.fromTo(finishScreen, {opacity: 1}, {
+          opacity: 0, pointerEvents: 'none', duration: 0.1
+        });
+        setHandlerState(current=> current = "PLAYING")
+        reset();
+      }
+      replayBtn.addEventListener('click', () => {
+        hidieFinishScreen();
+      });
+      const showReadyScreen =()=>{
+        gsap.to(titleImgae,{
+          scale: 1, rotation: 720, opacity: 1, duration: 0.5
+        })
+        gsap.to(playBtn, {
+          scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
+          //gsap Ease option 중 하나, elastic.easeOut.config(값이 변화되는 크기, 바운스되는 정도)
+        });
+      }
+      const showFinishScreen=()=>{
+        // distanceText.innerText = Math.floor(score.distCount) + 'm';
+        // coinText.innerText = score.coinCount + ' coin';
+        gsap.fromTo(finishScreen,{opacity: 0},{
+          opacity: 1, duration: 0.5, pointerEvents: 'all'
+        });
+    
+        gsap.fromTo(distanceText, {opacity: 0, scale: 0}, {
+          opacity: 1, scale: 1, duration: 0.5, delay: 1
+        });
+    
+        gsap.fromTo(coinText, {opacity: 0,  scale: 0},{
+          opacity: 1, scale: 1, duration: 0.5, delay: 1.1
+        });
+    
+        gsap.fromTo(replayBtn, {opacity: 0, scale: 0}, {
+          opacity: 1, scale: 1, rotation: 720, duration: 0.5, delay: 1.3
+        });
+      }
+      const handler = {
+        status : status,
+        showReadyScreen: ()=> showReadyScreen(),
+        showFinishScreen: ()=> showFinishScreen()
+      }
+
+      return handler; 
+    }
+
+
+
+
     const reset = () => {
       walls.push(createWall({type: 'SMALL'}));
       player = createPlayer(Math.random() > 0.3 ? 'blue-bird' : 'bird');
       coins = [];
-      scores = [];
+      score = [];
+    }
+
+    const createBoundingBox = (x, y, width, height) => {
+      const boundX = x;
+      const boundY = y;
+      const boundW = width;
+      const boundH = height;
+      const boundingBox = {
+          x : boundX,
+          y : boundY,
+          width : boundW,
+          height : boundH,
+          color : `rgba(255, 0, 0, 0.3)`,
+          isColliding: (target) => {
+            return (target.x + target.width >= boundX &&
+              target.x <= boundX + boundW && 
+              target.y + target.height >= boundY &&
+              target.y <= boundY + boundH)
+          }
+      }
+      return boundingBox;
     }
 
     const createBackground = (config) => {
@@ -93,14 +219,14 @@ const MiniGame = () => {
         case 'blue-bird':
           img.src = blueBird
           height = _width * (33 / 32);
-          // boundingBox = new BoundingBox(x + 1, y + 20, width - 9, height - 26 ); // 해당 상수는 img siz가 딱 그림에 맞지 않기 때문에 bound 조절 
+          boundingBox = createBoundingBox(_x + 1, _y + 20, _width - 9, height - 26 ); // 해당 상수는 img siz가 딱 그림에 맞지 않기 때문에 bound 조절 
           imgCnt = 9
           updateY= 20;
           break;
         case 'bird':
           img.src = bird
           height = _width * (96/140);
-          // boundingBox = new BoundingBox(x + 10, y + 16, width - 20, height - 20 ); // 해당 상수는 img siz가 딱 그림에 맞지 않기 때문에 bound 조절 
+          boundingBox = createBoundingBox(_x + 10, _y + 16, _width - 20, height - 20 ); // 해당 상수는 img siz가 딱 그림에 맞지 않기 때문에 bound 조절 
           imgCnt = 15;
           updateY= 16;
           break;
@@ -193,8 +319,8 @@ const MiniGame = () => {
       const y2 = y1 + height + gapY;
   
   
-      // const boundingBox1 = new BoundingBox(x + 30, y1 + 30, width - 60, height - 60);
-      // const boundingBox2 = new BoundingBox(x + 30, y2 + 30, width - 60, height - 60);
+      const boundingBox1 = createBoundingBox(x + 30, y1 + 30, width - 60, height - 60);
+      const boundingBox2 = createBoundingBox(x + 30, y2 + 30, width - 60, height - 60);
   
       const generatedNext = false;
       let gapNextX = canvasWidth * randomNumBetween(0.6, 0.75); // 벽 나오는 속도
@@ -211,8 +337,8 @@ const MiniGame = () => {
         sizeX:sizeX,
         width: width,
         height: height,
-        // boundingBox1: boundingBox1,
-        // boundingBox2: boundingBox2,
+        boundingBox1: boundingBox1,
+        boundingBox2: boundingBox2,
         generatedNext: generatedNext,
         gapNextX: gapNextX,
       }
@@ -247,7 +373,7 @@ const MiniGame = () => {
       
         if(delta < interval) return ;
 
-        // if(this.gameHandler.status !== 'PLAYING') return;
+        if(handlerStatus !== 'PLAYING') return;
 
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
       
@@ -258,8 +384,8 @@ const MiniGame = () => {
       //Wall
         for( let i = walls.length - 1 ; i >= 0; i--){
           walls[i].x +=  walls[i].vx
-          // this.boundingBox1.x = this.x + 30;
-          // this.boundingBox2.x = this.x + 30;
+          boundingBox1.x = walls[i].x + 30;
+          boundingBox2.x = walls[i].x + 30;
           ctx.drawImage(
             walls[i].img,
             walls[i].sx, 0, walls[i].img.width * walls[i].sizeX, walls[i].img.height,
@@ -293,10 +419,10 @@ const MiniGame = () => {
           }
 
           //player과 충돌
-          // if(this.walls[i].isColliding(this.player.boundingBox)){
-          //   this.gameHandler.status = 'FINISH'
-          //   break;
-          // }
+          if(walls[i].isColliding(player.boundingBox)){
+            setHandlerState(current => current = 'FINISH');
+            break;
+          }
         }
 
       //player
@@ -305,16 +431,16 @@ const MiniGame = () => {
 
       player.vy += player.gravity;
       player.y += player.vy;
-      // player.boundingBox.y = player.y + player.updateY;
+      player.boundingBox.y = player.y + player.updateY;
 
       ctx.drawImage(
         player.img,
         player.img.width / player.imgCnt * player.frameX, 0, player.img.width / player.imgCnt, player.img.height,
         player.x, player.y, player.width, player.height
       );
-      // if(player.player.y >= canvasHeight || player.player.y + player.player.height <= 0){
-      //   this.gameHandler.status = 'FINISH'
-      // }
+      if(player.y >= canvasHeight || player.y + player.height <= 0){
+        setHandlerState(current => current = 'FINISH');
+      }
 
       // //Coin
       // for( let i = this.coins.length -1 ; i >= 0; i--){
@@ -353,18 +479,17 @@ const MiniGame = () => {
 
   return (
       <div className="minigame-wrapper">
-        {/* <div className="minigame">
+        <div className="minigame">
           <section className="ready-screen">
-            <img src="./assets/title.png" className="title-img">
-            <img src="./assets/play.svg" className="play-img">
+           
           </section>
 
           <section className="finish-screen">
             <div className="distance">0m</div>
             <div className="coin">0coin</div>
-            <img src="./assets/replay.svg" className="replay-img">
+           
           </section>
-        </div>  */}
+        </div> 
 
         <canvas ref={canvasRef}></canvas>
       </div>
